eb8040526c6eaea3baedbe7bd4a2f379110b6340
tomi34 <66636695+tomi34@users.noreply.github.com>
2022-12-01 12:15:24 +0100

    pxt.json, main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,35 +1,44 @@
 input.onButtonPressed(Button.A, function () {
+    blublublubulu(50)
+})
+input.onButtonPressed(Button.AB, function () {
+    blublublubulu(1000)
+})
+input.onButtonPressed(Button.B, function () {
+    blublublubulu(100)
+})
+function blublublubulu (int: number) {
     for (let index = 0; index <= 4; index++) {
         led.plot(index, 0)
-        basic.pause(50)
+        basic.pause(int)
     }
     for (let index = 0; index <= 4; index++) {
         led.plot(4, index)
-        basic.pause(50)
+        basic.pause(int)
     }
     for (let index = 0; index <= 4; index++) {
         led.plot(4 - index, 4)
-        basic.pause(50)
+        basic.pause(int)
     }
     for (let index = 0; index <= 3; index++) {
         led.plot(0, 3 - index)
-        basic.pause(50)
+        basic.pause(int)
     }
     for (let index = 0; index <= 2; index++) {
         led.plot(1 + index, 1)
-        basic.pause(50)
+        basic.pause(int)
     }
     for (let index = 0; index <= 1; index++) {
         led.plot(3, 2 + index)
-        basic.pause(50)
+        basic.pause(int)
     }
     for (let index = 0; index <= 1; index++) {
         led.plot(2 - index, 3)
-        basic.pause(50)
+        basic.pause(int)
     }
     led.plot(1, 2)
-    basic.pause(50)
+    basic.pause(int)
     led.plot(2, 2)
-    basic.pause(5000)
+    basic.pause(int * 100)
     basic.clearScreen()
-})
+}
